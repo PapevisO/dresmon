ea58ba9c4a4fb61d9462204762d8517035b8af16
Oleksandr Papevis <sasha@papevis.com>
2023-10-27 14:04:59 +0200

    extract config & logger

--- dresmon.py
+++ dresmon.py
@@ -1,27 +1,14 @@
-import os
 import docker
 import importlib
 import pkgutil
-import logging
 from providers import BaseProvider
-
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'info').upper()
-# Set up logging
-logging.basicConfig(level=getattr(logging, LOG_LEVEL))
-logger = logging.getLogger(__name__)
-
-# Initialize environment variables
-DRESMON_PROVIDER = os.environ.get('DRESMON_PROVIDER', 'traefik')
-DRESMON_TLD = os.environ.get('DRESMON_TLD', 'local').split(',')
-logger.warn(f"Not implemented: DRESMON_TLD")
-DRESMON_EXCLUDE = os.environ.get('DRESMON_EXCLUDE', '').split(',')
-logger.warn(f"Not implemented: DRESMON_EXCLUDE")
-DRESMON_TARGET_IP = os.environ.get('DRESMON_TARGET_IP', '127.0.0.1')
-logger.warn(f"Not implemented: DRESMON_TARGET_IP")
-DRESMON_HOSTS_BLOCK_TAG = os.environ.get('DRESMON_HOSTS_BLOCK_TAG', 'dresmon').split(',')
-logger.warn(f"Not implemented: DRESMON_HOSTS_BLOCK_TAG")
+from config import logger, DRESMON_PROVIDER
 
 client = docker.from_env()
+
+###
+# Container events data provider
+###
 
 # Dynamically load all providers from the providers directory
 package_name = 'providers'
@@ -36,7 +23,11 @@ if not provider_class:
 
 provider_instance = provider_class()
 
-def update_hosts(container):
+###
+# Define algorythm 
+###
+
+def process_container(container):
     domains = provider_instance.get_domains(container)
     if domains:
         logger.debug(f"Domains for container {container.name}: {', '.join(domains)}")
@@ -55,7 +46,10 @@ def monitor_docker_events():
         if event['Type'] == 'container' and event['Action'] == 'start':
             container = client.containers.get(event['id'])
             logger.debug(f"New container started: {container.name}")
-            update_hosts(container)
+
+###
+# Start and listen for container events
+###
 
 if __name__ == "__main__":
     process_existing_containers()
